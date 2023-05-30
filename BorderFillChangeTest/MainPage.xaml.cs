94b7e3f302a8ae0a2c6e5e8c188db7377b859b93
jimm <Jim@two-bits.net>
2023-05-30 15:32:44 -0700

    Add test code.

--- BorderFillChangeTest/MainPage.xaml.cs
+++ BorderFillChangeTest/MainPage.xaml.cs
@@ -2,24 +2,11 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage() {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    void Button_Clicked(System.Object sender, System.EventArgs e) {
+        Container.HorizontalOptions = Container.HorizontalOptions.Alignment == LayoutOptions.Fill.Alignment ? LayoutOptions.Start : LayoutOptions.Fill;
+    }
 }
-
-
