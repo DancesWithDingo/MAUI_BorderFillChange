1e88a76ff5dfbf607c4cd21db666ba94946015d6
jimm <Jim@two-bits.net>
2023-05-30 15:32:15 -0700

    initial commit

---
+++ BorderFillChangeTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BorderFillChangeTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ BorderFillChangeTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BorderFillChangeTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
